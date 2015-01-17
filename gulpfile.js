c26f9425be405e0bbbe65d60352423dd2768e075
Levi Lindsey <levisl176@gmail.com>
2015-01-17 14:49:01 -0800

    Updates GA tracking code.

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ config.analyticsScript =
       "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
       "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
     "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" +
-    "ga('create', 'UA-43971205-1', 'jackieandlevi.com');" +
+    "ga('create', 'UA-43971205-4', 'levi.sl');" +
     "ga('send', 'pageview');";
 
 // ---  --- //
