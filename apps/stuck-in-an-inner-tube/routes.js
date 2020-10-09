dbfcd0683ba85f408cb6cef427ece91fd11f2f65
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2020-10-08 21:12:12 -0700

    Add stuck-in-an-inner-tube post.

---
+++ apps/stuck-in-an-inner-tube/routes.js
@@ -0,0 +1,21 @@
+var routeRegex = /^\/(stuck-in-an-inner-tube|tube-climber|ld47|ludum-dare-47)(?:\/.*)?$/;
+
+var redirectUrl = 'https://levilindsey.github.io/stuck-in-an-inner-tube';
+
+// Attaches the route handlers for this app.
+exports.attachRoutes = function (server, appPath, config) {
+  server.get(routeRegex, handleSurfacerRequest);
+
+  // ---  --- //
+
+  // Handles a request for this app.
+  function handleSurfacerRequest(req, res, next) {
+    var dirs = req.path.split('/');
+
+    if (dirs[2] === '' || dirs.length === 2) {
+      res.redirect(redirectUrl);
+    } else {
+      next();
+    }
+  }
+};
