130a00609ecc9679669244fcb9be1cf36edbbfc8
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-02-11 21:20:23 -0800

    Add content to snoring cat

--- apps/snoring-cat/public/scripts/main.js
+++ apps/snoring-cat/public/scripts/main.js
@@ -6,8 +6,9 @@
  * @module main
  */
 (function () {
+  var CARD_OPEN_DURATION = 200;
 
-  window.addEventListener('load', init, false);
+  window.addEventListener('load', init);
 
   // ---  --- //
 
@@ -19,7 +20,27 @@
 
     window.removeEventListener('load', init);
 
-    var dashboard = document.querySelector('#dashboard');
+    var cards = document.querySelectorAll('.card');
+    cards.forEach(function (card) {
+      var cardHeaders = card.querySelectorAll('.card-header-wrapper');
+      var cardBodies = card.querySelectorAll('.card-body-wrapper');
+      if (cardHeaders.length !== 1 || cardBodies.length !== 1) {
+        console.error('Invalid card structure');
+      }
+      cardHeaders[0].addEventListener('click', onHeaderClick.bind(null, card));
+    });
+  }
+
+  function onHeaderClick(card) {
+    card.classList.add('transitioning');
+    if (card.classList.contains('open')) {
+      card.classList.remove('open');
+    } else {
+      card.classList.add('open');
+    }
+    setTimeout(function () {
+      card.classList.remove('transitioning');
+    }, CARD_OPEN_DURATION);
   }
 
   console.log('main module loaded');
