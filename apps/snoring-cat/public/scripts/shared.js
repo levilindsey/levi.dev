37a800c5d35e001a67b8d43e61d77153e6d575e8
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-02-13 20:46:06 -0800

    Add client-side routing.

--- apps/snoring-cat/public/scripts/shared.js
+++ apps/snoring-cat/public/scripts/shared.js
@@ -6,6 +6,13 @@
 (function () {
   var CARD_OPEN_DURATION = 200;
 
+  var path = location.pathname;
+  while (path.length > 0 && path[path.length - 1] === '/') {
+    path = path.substr(0, path.length - 1);
+  }
+
+  var openCard = null;
+
   window.addEventListener('load', init);
 
   function init() {
@@ -22,18 +29,37 @@
       }
       cardHeaders[0].addEventListener('click', onHeaderClick.bind(null, card));
     });
+
+    openCardMatchingHash();
   }
 
   function onHeaderClick(card) {
     card.classList.add('transitioning');
     if (card.classList.contains('open')) {
+      openCard = null;
       card.classList.remove('open');
+      history.pushState({}, document.title, path + '/');
     } else {
+      if (!!openCard) {
+        openCard.classList.remove('open');
+      }
+      openCard = card;
       card.classList.add('open');
+      history.pushState({}, document.title, path + '#' + card.id);
     }
     setTimeout(function () {
       card.classList.remove('transitioning');
     }, CARD_OPEN_DURATION);
+  }
+
+  function openCardMatchingHash() {
+    if (location.hash.length > 1) {
+      var cardId = location.hash.substr(1);
+      var matchingCard = document.querySelector('#' + cardId + '.card');
+      if (!!matchingCard) {
+        matchingCard.classList.add('open');
+      }
+    }
   }
 
   console.log('shared module loaded');
