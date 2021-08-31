2cd77666490017e6f2f1aa165a3a1a401b2e4e0b
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2021-08-31 15:24:15 -0700

    Add a path for ludum dare 49.

---
+++ apps/ludum-dare-49/routes.js
@@ -0,0 +1,34 @@
+// TODO: Add paths based on the game name.
+const routeRegex = /^\/(ld49|ludum-dare-49|ludumdare49)(?:\/.*)?$/i;
+
+const githubPagesUrl = 'levilindsey.github.io/ludum-dare-49/';
+
+const itchioUrl = 'https://levilindsey.itch.io/TODO';
+
+// TODO: Use the itch.io URL.
+const demoUrl = githubPagesUrl;
+// const demoUrl = itchioUrl;
+
+// Attaches the route handlers for this app.
+exports.attachRoutes = (server, appPath, config) => {
+  server.get(routeRegex, handleRequest);
+
+  // ---  --- //
+
+  // Handles a request for this app.
+  function handleRequest(req, res, next) {
+    // Check whether this request was directed to the portfolio.
+    if (config.portfolioDomains.indexOf(req.hostname) < 0) {
+      next();
+      return;
+    }
+
+    const dirs = req.path.split('/');
+
+    if (dirs[2] === '' || dirs.length === 2) {
+      res.redirect(demoUrl);
+    } else {
+      next();
+    }
+  }
+};
