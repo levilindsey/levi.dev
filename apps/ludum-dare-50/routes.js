2591925aab033056a40b6e7a200dca7ce22d96fc
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-04-01 09:58:39 -0700

    Add a route for ludum-dare-50.

---
+++ apps/ludum-dare-50/routes.js
@@ -0,0 +1,33 @@
+// TODO: Add the app name.
+const routeRegex = /^\/(ld50|ludum-?dare-?50)(?:\/.*)?$/i;
+
+const githubPagesUrl = 'https://levilindsey.github.io/ludum-dare-50/';
+
+// TODO: Add the app name.
+const itchioUrl = 'https://levilindsey.itch.io/';
+
+const demoUrl = githubPagesUrl;
+
+// Attaches the route handlers for this app.
+exports.attachRoutes = (server, appPath, config) => {
+  server.get(routeRegex, handleRequest);
+
+  // ---  --- //
+
+  // Handles a request for this app.
+  function handleRequest(req, res, next) {
+    // Check whether this request was directed to the portfolio.
+    if (config.portfolioDomains.indexOf(req.hostname) < 0) {
+      next();
+      return;
+    }
+
+    const dirs = req.path.split('/');
+
+    if (dirs[2] === '' || dirs.length === 2) {
+      res.redirect(demoUrl);
+    } else {
+      next();
+    }
+  }
+};
