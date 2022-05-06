ddbb20b11e0e81390b528b55a7790213b9fdc309
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-05-06 13:47:46 -0700

    Distinguish between meteor-power and ludum-dare-50 routes.

--- apps/ludum-dare-50/routes.js
+++ apps/ludum-dare-50/routes.js
@@ -1,9 +1,6 @@
-// TODO: Add the app name.
-const routeRegex = /^\/(ld50|ludum-?dare-?50|meteor(-?power)?)(?:\/.*)?$/i;
+const routeRegex = /^\/(ld50|ludum-?dare-?50)(?:\/.*)?$/i;
 
 const githubPagesUrl = 'https://levilindsey.github.io/ludum-dare-50/';
-
-const itchioUrl = 'https://levilindsey.itch.io/meteor-power';
 
 const demoUrl = githubPagesUrl;
 
