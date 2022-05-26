69ebebd0ac3d915819ff5b0ab67c787c365df7bb
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-05-26 12:54:42 -0700

    Add a showreel route.

---
+++ apps/showreel/routes.js
@@ -0,0 +1,28 @@
+const routeRegex = /^\/((game(play)?)?-?show-?reel)(?:\/.*)?$/i;
+
+const youtubeUrl = 'https://www.youtube.com/watch?v=XCHBPZsOKGw';
+const portfolioUrl = 'https://levi.dev#showreel';
+
+// Attaches the route handlers for this app.
+exports.attachRoutes = (server, appPath, config) => {
+  server.get(routeRegex, handleRequest);
+
+  // ---  --- //
+
+  // Handles a request for this app.
+  function handleRequest(req, res, next) {
+    // Check whether this request was directed to the portfolio.
+    if (config.portfolioDomains.indexOf(req.hostname) < 0) {
+      next();
+      return;
+    }
+
+    const dirs = req.path.split('/');
+
+    if (dirs[2] === '' || dirs.length === 2) {
+      res.redirect(portfolioUrl);
+    } else {
+      next();
+    }
+  }
+};
